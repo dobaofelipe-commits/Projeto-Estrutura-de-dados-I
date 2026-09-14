f1dc72262410e310bfbfad4b7177da2717b8a190
Felipe Cavallo <dobaofelipe@gmail.com>
2026-09-13 23:57:34 -0300

    Criando função enfila e desenfila Aula

--- sistemaEscolar.cpp
+++ sistemaEscolar.cpp
@@ -4,6 +4,10 @@
 #include <string>
 using namespace std;
 
+int inicioAula[5]={-1,-1,-1,-1,-1}, fimAula[5]={-1,-1,-1,-1,-1};
+string aulas[5][4]; //primeiro o dia e depois a sequencia de turmas
+
+
 
 struct Alunos{
 string nome;
@@ -27,22 +31,23 @@ string nome;
 string codigo;
 string telefone;
 string emailInstitucional;
-string turmas[5][4]; //primeiro o dia e depois a sequencia de turmas
 };
 
 
 //funÇões relacionadas a TURMAS
 
-void cadastarTurmas(Turmas &turma){
-    cout<<"Qual o nome da turma: ";
-    getline(cin, turma.nome);
-    turma.codigo=gerarCodigoTurma();   
-}
 
 string gerarCodigoTurma(){
 string codigo;
 codigo = "T" + to_string(rand() % 9000 + 1000);
 return codigo;
+}
+
+
+void cadastarTurmas(Turmas &turma){
+    cout<<"Qual o nome da turma: ";
+    getline(cin, turma.nome);
+    turma.codigo=gerarCodigoTurma();   
 }
 
 
@@ -78,8 +83,10 @@ do{
 
 }while(contador<2);
 return email;
-}
+} // fim gerar email-professor
 
+
+//função de cadastrar o professor
 void cadastrarProfessor(Professor &professor){
 
     cout<<"Qual o nome do professor: ";
@@ -89,9 +96,71 @@ void cadastrarProfessor(Professor &professor){
     professor.codigo=gerarCodigoProfessor();
     professor.emailInstitucional=gerarEmail(professor.nome);
 
+} // fim função de cadastrar o professor
+
+void enfilaAula(int dia, string turma, int numProfessor){
+
+if(fimAula[numProfessor]<4){
+fimAula[numProfessor-1]++;
+aulas[dia][fimAula[numProfessor-1]]=turma;
+cout<<fimAula[numProfessor-1]<< "\n";
+} else {
+    cout<<"A quantidade de materias para esse dia ja está cheia";
+}
+
+if(inicioAula[numProfessor-1]==-1){
+    inicioAula[numProfessor-1]=0;
+}
+
+} //Fim enfila 
+
+string desenfilaAula(int dia, int numProfessor){
+    string resposta;
+    if(inicioAula[numProfessor-1]>=0){
+        resposta=aulas[dia][inicioAula[numProfessor-1]];
+        if(inicioAula[numProfessor-1]!=fimAula[numProfessor-1]){
+            cout<<inicioAula[numProfessor-1]<< "\n";
+            inicioAula[numProfessor-1]++;
+        }else{
+            inicioAula[numProfessor-1]=0;
+            fimAula[numProfessor-1]=0;
+        }
+
+    } else{
+        cout<<"Esse dia está vazio, adicione aulas para esse professor";
+    }
+    return resposta;
 }
 
 
 int main(){
+
+    int dia, numProfessor, opcao;
+    string nomeTurma;
+
+    do{
+    cout<<"Escolha o dia ";
+    cin>>dia;
+    cout<<"Escolha a turma ";
+    cin>>nomeTurma;
+    cout<<"Escolha o Professor ";
+    cin>>numProfessor;
+    enfilaAula(dia, nomeTurma, numProfessor);
+    cout<<"2 para parar";
+    cin>>opcao;
+
+    }while(opcao!=2);
+    
+    cout<<"Escolha o dia que quer saber a sequencia \n";
+    cin>>dia;
+    cout<<"sequencia de turma é \n";
+    for(int i=0;i<4;i++){
+        cout<<desenfilaAula(dia,numProfessor);
+        i++;
+    }
+    cout<<aulas[dia][2]<<"\n";
+    cout<<aulas[dia][3];
+
+
 
 }
